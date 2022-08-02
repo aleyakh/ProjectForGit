0cf52c8367e479cc5e514f1b8a64d4ce21876290
aleyakh <108677447+aleyakh@users.noreply.github.com>
2022-08-03 00:43:10 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def hello(name):
     print(f'Hello {name}!')
     print('I am using github right now!')
-
+    print('This string was create in GitHub. I need to pull it in my local laptop.')
 
 hello("Misha")
